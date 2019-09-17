987eac805712d82513d09b71cfa22ed6c5e9eb35
Matthew Robinson <matt_robinson1@yahoo.com>
2019-09-17 14:21:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,26 +1,39 @@
-  
 class Dog
-  attr_accessor :name
+
+  attr_reader :name
 
   @@all = []
 
   def initialize(name)
     @name = name
-    @@all << self
+    save
   end
 
   def self.all
-    @@all.each do |dog|
-      puts dog.name
-    end
+    @@all
   end
 
   def self.clear_all
     @@all.clear
   end
 
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name
+    end
+
+  end
+
+  # private
+
+    def save
+      @@all << self
+    end
+
+
+
+
 end
-  
   
 
   
